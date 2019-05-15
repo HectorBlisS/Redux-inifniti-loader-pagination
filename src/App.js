4e32eead8891a74a3b7e5434e858da5fa0f8d296
BlisS <contacto@fixter.org>
2019-05-15 14:24:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Paginated from './components/Paginated';
 function App() {
   return (
     <div className="App">
-      <h1>Bienvenidos al Nolte Meetup</h1>
+      <h2>Que ño!!</h2>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Route path="/profile" component={Profile} />
